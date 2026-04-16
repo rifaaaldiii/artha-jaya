33316dd6102c8d55df7b32d889dfc42935feface
Rifaldi <rifaldiyuda29@gmail.com>
2026-04-16 13:25:39 +0700

    Update

--- app/Filament/Resources/Pelanggans/PelangganResource.php
+++ app/Filament/Resources/Pelanggans/PelangganResource.php
@@ -11,6 +11,7 @@ use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
 
 class PelangganResource extends Resource
